ae0d2fbb314f27eda0ec34d100f156caade5f88c
Jean Marie Cimula <jmkacim@gmail.com>
2016-08-06 12:56:58 +0200

    List of sectors

--- sector.R
+++ sector.R
@@ -1,14 +1,14 @@
 #Sectors
-climate_change <- "climate-change"
-agriculture    <- "agriculture-agro-industries"
-economic       <- "economic-financial-governance"
-education      <- "education"
-energy         <- "energy-power"
-environmentt   <- "environment"
-human          <- "human-capital-development"
-health         <- "health"
-information    <- "information-communication-technology"
-infrastructure <- "infrastructure"
-gender    <- "gender"
-transport <- "transport"
-water     <- "water-supply-sanitation"
+sector <- function() {
+
+climate_change <- "climate-change";agriculture <- "agriculture-agro-industries"
+economic <- "economic-financial-governance";education <- "education"
+energy <- "energy-power";environmentt <- "environment";human <- "human-capital-development"
+health<- "health";information <- "information-communication-technology";infrastructure <- "infrastructure"
+gender <- "gender";transport <- "transport";water <- "water-supply-sanitation"
+
+st <- c(climate_change,agriculture,economic,education,energy,environmentt,human,health,information,infrastructure,gender,transport,water)
+print(st)
+}
+
+#> sector()
